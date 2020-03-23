a8102cd3c7ce2b3de12aed18b195f75619a6065f
kimiti <kimitikevin@gmail.com>
2020-03-23 11:02:02 +0300

    update bloc music_player dart file

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -3,14 +3,6 @@ import 'package:flutter/material.dart';
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        sliderTheme: SliderThemeData(
-          trackHeight: 1,
-        ),
-      ),
-      home: Text("hello world"),
-    );
+    return Container();
   }
 }
--- lib/src/blocs/music_player.dart
+++ lib/src/blocs/music_player.dart
@@ -24,4 +24,65 @@ class MusicPlayerBloc {
   BehaviorSubject<Duration> get position$ => _position$;
   BehaviorSubject<List<Playback>> get playback$ => _playback$;
   BehaviorSubject<List<Song>> get favorites$ => _favorites$;
+
+  MusicPlayerBloc() {
+    _initDefultSong();
+    _initStreams();
+    _initObservers();
+    _initAudioPlayer();
+  }
+
+  // fetch all songs
+  // add songs to List
+  Future<void> fetchSongs() async {
+    await MusicFinder.allSongs().then((data) {
+      _songs$.add(data);
+    });
+  }
+
+// update state of music player {playing, paused, stopped,}
+  void updatePlayerState(PlayerState state, Song song) {
+    _playerState$.add(MapEntry(state, song));
+  }
+
+  // Play Song
+  void playMusic(Song song) {
+    _audioPlayer.play(song.uri);
+    updatePlayerState(PlayerState.playing, song);
+  }
+
+  // pause music
+  void pauseMusic(Song song) {
+    _audioPlayer.pause();
+    updatePlayerState(PlayerState.paused, song);
+  }
+
+// stop the music
+  void stopMusic() {
+    _audioPlayer.stop();
+  }
+
+// update position
+  void updatePostion(Duration duration) {
+    _position$.add(duration);
+  }
+
+  // update playlist
+  void updatePlaylist(List<Song> normalPlaylist) {
+    List<Song> _shufflePlaylist = []..addAll(normalPlaylist);
+    _shufflePlaylist.shuffle();
+    _playlist$.add(MapEntry(normalPlaylist, _shufflePlaylist));
+  }
+
+  // update album
+  void updateAlbum(List<Song> songs) {
+    Map<int, Album> _albumsMap = {};
+    for (Song song in songs) {
+      if (_albumsMap[song.albumId] == null) {
+        _albumsMap[song.albumId] = Album.fromSong(song);
+      }
+    }
+    final List<Album> _albums = _albumsMap.values.toList();
+    _albums$.add(_albums);
+  }
 }
